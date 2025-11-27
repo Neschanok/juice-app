cf789626e7c961dd009bf9eaaad16a61d69bc229
MaikenGroenbech <maiken.2001@live.dk>
2025-11-27 15:38:02 +0100

    tilføjet kalenderen

--- public/kalender.js
+++ public/kalender.js
@@ -4,6 +4,9 @@ document.addEventListener("DOMContentLoaded", function() {
     dageContainer = document.querySelector(".dage"),
     prev = document.querySelector(".prev"),
     next = document.querySelector(".next");
+    idagBtn = document.querySelector(".idag-btn");
+    gotoBtn = document.querySelector(".goto-btn");
+    dateInput = document.querySelector(".date-input");
 
 
 let today = new Date();
@@ -79,6 +82,71 @@ function inputKalender() {
 }
 
 inputKalender();
+
+//Forrige måned
+function prevMonth() {
+    month--;
+    if (month < 0) {
+        month = 11;
+        year--;
+    }
+    inputKalender();
+}
+//Næste måned
+function nextMonth() {
+    month++;
+    if (month > 11) {
+        month = 0;
+        year++; 
+    }
+    inputKalender();
+}
+
+//tilføjer eventlistener på forrige og næste måneds pointing arrow icon
+prev.addEventListener("click", prevMonth)
+next.addEventListener("click", nextMonth)
+
+//Tilføjer goto date og goto idag funktionaliteter
+idagBtn.addEventListener("click", () => {
+    today = new Date();
+    month = today.getMonth();
+    year = today.getFullYear();
+    inputKalender();
+})
+
+dateInput.addEventListener("input", (e) => {
+    //Tillad kun tal - fjern alt andet
+    dateInput.value = dateInput.value.replace(/[^0-9]/g, "");
+
+    if (dateInput.value.length > 2 && dateInput.value[2] !== "/") {  
+    //tilføj '/', hvis to tal er indtastet
+        dateInput.value = dateInput.value.slice(0,2) +  "/" + dateInput.value.slice(2);
+    }
+
+    if (dateInput.value.length > 7) {
+        dateInput.value = dateInput.value.slice(0, 7)
+    }
+
+})
+
+gotoBtn.addEventListener("click", gotoDate);
+
+//funktion til at få til den indtastede dato
+function gotoDate() {
+    const dateArr = dateInput.value.split("/");
+    if (dateArr.length === 2) {
+        if (dateArr[0] > 0 && dateArr[0] < 13 && dateArr[1].length === 4) {
+            month = dateArr[0] - 1;
+            year = parseInt(dateArr[1]);
+            inputKalender();
+            return;
+        }
+    }
+    //hvis dato er ugyldig
+    alert("invalid date");
+}
+
 });
+
 
 console.log("JS virker!");
